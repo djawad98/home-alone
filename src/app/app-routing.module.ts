0510960d6e55003ed23a3c218c72025929e0aabd
djawad <ghased.omp@gmail.com>
2023-07-28 15:24:37 +0330

    add banner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelectGenderComponent } from './pages/select-gender/select-gender.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'gender',
+    component: SelectGenderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
