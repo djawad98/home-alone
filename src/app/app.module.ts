a26fcbeb49b0d36627a4cf220f3b4dff5633377a
djawad <ghased.omp@gmail.com>
2023-07-28 15:06:56 +0330

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NzButtonModule} from 'ng-zorro-antd/button'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
